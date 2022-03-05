45228a42b2357506a53106684d1a1fe5b4127459
Moomallowz <72464266+Moomallowz@users.noreply.github.com>
2022-03-04 23:53:44 -0600

    scrap and refactor

--- src/main/java/com/moomallowz/sinker/core/init/ItemReg.java
+++ src/main/java/com/moomallowz/sinker/core/init/ItemReg.java
@@ -26,7 +26,5 @@ public class ItemReg {
 	public static final RegistryObject<Item> SHUCKED_MUSSEL = ITEMS.register("shucked_mussel", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).food(new FoodProperties.Builder().nutrition(2).saturationMod(4.0f).effect(() -> new MobEffectInstance(MobEffects.HUNGER, 600, 0), 0.2f).build())));
 	public static final RegistryObject<Item> BOILED_MUSSEL = ITEMS.register("boiled_mussel", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).food(new FoodProperties.Builder().nutrition(5).saturationMod(6.0f).build())));
 	public static final RegistryObject<Item> CLAM_CHOWDER = ITEMS.register("clam_chowder", () -> new BowlItem(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).craftRemainder(Items.BOWL).stacksTo(1).food(new FoodProperties.Builder().nutrition(10).saturationMod(12.0f).effect(() -> new MobEffectInstance(MobEffects.LUCK, 600, 0), 1.0f).build())));
-	public static final RegistryObject<Item> WHITE_PEARL = ITEMS.register("white_pearl", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
-	public static final RegistryObject<Item> BLACK_PEARL = ITEMS.register("black_pearl", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
-	public static final RegistryObject<Item> PINK_PEARL = ITEMS.register("pink_pearl", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+	public static final RegistryObject<Item> PEARL = ITEMS.register("pearl", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 }
